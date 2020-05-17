e4ef3345cb263613700b93b3f0f53a7d1c2fb00d
Serpel <serpel.js@gmail.com>
2020-05-16 18:43:28 -0600

    Initial Project

---
+++ StringCalculatorMaaiHam/StringCalculatorMaaiHam.UnitTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringCalculatorMaaiHam.UnitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
